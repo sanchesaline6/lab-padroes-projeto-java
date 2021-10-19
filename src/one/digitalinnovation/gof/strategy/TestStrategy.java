dc1d005c407ea7285eaaa05fde3b9296adf28376
Aline Sanches <sanches.aline6@gmail.com>
2021-10-19 09:19:26 -0300

    Implements examples of Statregy design pattern

---
+++ src/one/digitalinnovation/gof/strategy/TestStrategy.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof.strategy;
+
+public class TestStrategy {
+
+    public static void main(String[] args) {
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+    }
+}
