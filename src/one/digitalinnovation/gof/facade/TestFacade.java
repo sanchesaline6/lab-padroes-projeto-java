fc9ab17dea340fa5784ab9a4f184828281a9cdbf
Aline Sanches <sanches.aline6@gmail.com>
2021-10-19 09:51:50 -0300

    Implements examples of Facade design pattern

---
+++ src/one/digitalinnovation/gof/facade/TestFacade.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.gof.facade;
+
+public class TestFacade {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Aline", "77019518");
+
+    }
+}
